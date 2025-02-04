a1be7b5a6c80620f464d6e1b954a1d03d9bcb8df
ganeshchinna7 <ganeshchinna777@gmail.com>
2025-02-04 12:49:41 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,405 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-firstfloorcrinche-001",
+      "name": "FirstFloorCrinche-001",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0584193813029295,
+          "pitch": 0.02882958524145529,
+          "rotation": 0,
+          "target": "3-firstfloorlobby-001"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-firstfloorgym-001",
+      "name": "FirstFloorGym-001",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.464634774376531,
+          "pitch": 0.01705681285095828,
+          "rotation": 0,
+          "target": "3-firstfloorlobby-001"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-firstfloorindoorgames-001",
+      "name": "FirstFloorIndoorGames-001",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.047135991790265,
+          "pitch": 0.029793910465498286,
+          "rotation": 0,
+          "target": "3-firstfloorlobby-001"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-firstfloorlobby-001",
+      "name": "FirstFloorLobby-001",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.229967508325881,
+          "pitch": 0.08336111944309188,
+          "rotation": 0,
+          "target": "2-firstfloorindoorgames-001"
+        },
+        {
+          "yaw": 2.357401466734184,
+          "pitch": 0.08783302007557836,
+          "rotation": 0,
+          "target": "1-firstfloorgym-001"
+        },
+        {
+          "yaw": 2.7667415315205437,
+          "pitch": -0.0009495970735855508,
+          "rotation": 0,
+          "target": "0-firstfloorcrinche-001"
+        },
+        {
+          "yaw": -2.7405068949694105,
+          "pitch": 0.0649743764653028,
+          "rotation": 0,
+          "target": "1-firstfloorgym-001"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-firstflooryoga-001",
+      "name": "FirstFloorYoga-001",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.270084048950345,
+          "pitch": 0.04408133877158349,
+          "rotation": 0,
+          "target": "3-firstfloorlobby-001"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-groundbanquethall-001",
+      "name": "GroundBanquetHall-001",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7713603921456915,
+          "pitch": 0.0023732024195819434,
+          "rotation": 0,
+          "target": "7-groundfloorlobby-001"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-groundbanquethall-002",
+      "name": "GroundBanquetHall-002",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.675644432602743,
+          "pitch": 0.09938828201287464,
+          "rotation": 0,
+          "target": "7-groundfloorlobby-001"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-groundfloorlobby-001",
+      "name": "GroundFloorLobby-001",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.839571825635236,
+          "pitch": 0.0650902823672741,
+          "rotation": 0,
+          "target": "5-groundbanquethall-001"
+        },
+        {
+          "yaw": -0.21782843412767505,
+          "pitch": 0.023794811051274678,
+          "rotation": 0,
+          "target": "6-groundbanquethall-002"
+        },
+        {
+          "yaw": 0.09000388108778523,
+          "pitch": 0.016390714857260846,
+          "rotation": 0,
+          "target": "9-groundfloorreception-001"
+        },
+        {
+          "yaw": -2.9911990399404402,
+          "pitch": 0.08056916191896235,
+          "rotation": 0,
+          "target": "8-groundfloorlobby-002"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-groundfloorlobby-002",
+      "name": "GroundFloorLobby-002",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": -1.9482145693016122,
+        "pitch": -0.09801782307893347,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1598077689820272,
+          "pitch": 0.044599673744791346,
+          "rotation": 0,
+          "target": "3-firstfloorlobby-001"
+        },
+        {
+          "yaw": -2.8540351042599745,
+          "pitch": -0.035840044010745586,
+          "rotation": 0,
+          "target": "3-firstfloorlobby-001"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-groundfloorreception-001",
+      "name": "GroundFloorReception-001",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": 1.6392742533282068,
+        "pitch": 0.07582963639087481,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.908217573131356,
+          "pitch": 0.13412817225547968,
+          "rotation": 0,
+          "target": "6-groundbanquethall-002"
+        },
+        {
+          "yaw": 3.0658186277329627,
+          "pitch": 0.06907764570867236,
+          "rotation": 0,
+          "target": "7-groundfloorlobby-001"
+        },
+        {
+          "yaw": 2.6851259277886443,
+          "pitch": 0.04930198827468324,
+          "rotation": 0,
+          "target": "5-groundbanquethall-001"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Youtube 360",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
